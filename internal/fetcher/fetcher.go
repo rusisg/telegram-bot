f1c9cf0a0debe9a3d65309fd32e47b04f0fef3f9
Rasul <rasul.nulrybekov@gmail.com>
2024-10-16 12:16:35 +0200

    fetcher

---
+++ internal/fetcher/fetcher.go
@@ -0,0 +1,43 @@
+package fetcher
+
+import (
+	"context"
+	"news_feed_bot/internal/model"
+	"time"
+)
+
+type ArticleStorage interface {
+	Store(ctx context.Context, article model.Article) error
+}
+
+type SourceProvider interface {
+	Sources(ctx context.Context) ([]model.Source, error)
+}
+
+type Source interface {
+	ID() int64
+	Name() string
+	Fetch(ctx context.Context) ([]model.Item, error)
+}
+
+type Fetcher struct {
+	articles ArticleStorage
+	sources  SourceProvider
+
+	fetchInterval  time.Duration
+	filterKeywords []string
+}
+
+func New(
+	articleStorage ArticleStorage,
+	sourceProvider SourceProvider,
+	fetchInterval time.Duration,
+	filterKeywords []string,
+) *Fetcher {
+	return &Fetcher{
+		articles:       articleStorage,
+		sources:        sourceProvider,
+		fetchInterval:  fetchInterval,
+		filterKeywords: filterKeywords,
+	}
+}
